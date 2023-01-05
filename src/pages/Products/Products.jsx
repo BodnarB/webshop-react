80c8c9183be00985f6a75bbca6f644b70acb0e4d
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-01-05 15:55:06 +0100

    custom select menu

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import ProductCard from '../../Components/ProductCard/ProductCard'
 import './Products.css'
+import { useState } from 'react'
 
 export default function Products({ addToCart }) {
+
+    const [selected, setSelected] = useState('Price: Low to high')
+    const [showSelect, setShowSelect] = useState('hide')
+
+    function selectMenu() {
+        showSelect === '' ? setShowSelect('hide') : setShowSelect('')
+    }
+
+    function setSelect(e) {
+        setSelected(e.target.innerText)
+        setShowSelect('hide')
+    }
+
     return (
         <div className='products-page'>
             <h2 className='prod-h2'>Products</h2>
-            <select className='sorting-products' name="order" id="order">
-                <option value="price-low">Price: Low to high</option>
-                <option value="price-high">Price: High to low</option>
-                <option value="a-z">A-Z</option>
-            </select>
+            <div className='custom-select' >
+                <button className='select-btn' onClick={selectMenu}>Sort by: {selected}</button>
+                <ul className={`products-sorting ${showSelect}`}>
+                    <li onClick={setSelect} className='sorting-mode'>Price: Low to high</li>
+                    <li onClick={setSelect} className='sorting-mode'>Price: High to low</li>
+                    <li onClick={setSelect} className='sorting-mode'>A-Z</li>
+                </ul>
+            </div>
+
             <div className='products-container'>
                 <ProductCard
                     imgSrc={"\\Assets\\Trendings\\pexels-abhinav-goswami-291528.jpg"}
