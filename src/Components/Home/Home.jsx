35f4cbe79e26c0deaa7586eb860744f2ea0cea9c
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-01-15 13:57:43 +0100

    save before git deploy

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,6 +11,7 @@ export default function Home({ addToCart, prodTitle }) {
                 <img className='intro-img' src={illustration} alt="" />
             </div>
             <div className='home-intro-container'>
+                <img className='intro-shop-img' src="https://images.unsplash.com/photo-1491222825723-0b8308840432?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2154&q=80" alt="" />
                 <div>
                     <p className='intro-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                         Exercitationem, harum! Rem optio quis expedita quam repellat labore sunt?
@@ -18,7 +19,6 @@ export default function Home({ addToCart, prodTitle }) {
                     <p className="intro-text">Iusto eum a nam rem. Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Tempore ullam ducimus adipisci magni quos, cupiditate labore. Quae ex nisi distinctio?</p>
                 </div>
-                <img className='intro-shop-img' src="https://images.unsplash.com/photo-1491222825723-0b8308840432?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2154&q=80" alt="" />
             </div>
             <section className='trendings-section'>
                 <Trendings addToCart={addToCart} prodTitle={prodTitle} />
