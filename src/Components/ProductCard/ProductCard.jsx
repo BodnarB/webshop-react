bf31139681dc895368b18798b89416a555c71a48
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-01-15 16:31:11 +0100

    try

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -6,7 +6,9 @@ export default function ProductCard({ imgSrc, prodTitle, prodPrice, addToCart })
 
     return (
         <div className='prod-card'>
-            <img className='trending-img' src={process.env.PUBLIC_URL + imgSrc} alt="" />
+            <img className='trending-img'
+                src={'.' + process.env.PUBLIC_URL + imgSrc}
+                alt="" />
             <div className='prod-info-box'>
                 <p className='prod-title'>{prodTitle}</p>
                 <p className='prod-price'>{prodPrice} $</p>
